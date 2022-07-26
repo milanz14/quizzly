c9b80a6fe22d6c384e93110c866f50b605cf0747
Milan Zagorac <zagoracm@gmail.com>
2022-07-26 10:10:16 -0400

    save Result component

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="result">
     <div class="title">You got {{ result }} correct!</div>
-    <div class="desc">{{ result < 3 ? results[0].desc : results[1].desc }}</div>
+    <div class="desc">
+      {{ result < 10 ? results[0].desc : results[1].desc }}
+    </div>
   </div>
 </template>
 
